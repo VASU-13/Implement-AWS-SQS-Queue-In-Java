aa1053858e2dad34a9fb536cdd7892a4da1351e2
VASU-13 <sharma.vasu1997@gmail.com>
2022-01-27 10:55:33 +0530

    implemented-aws-sqs

---
+++ src/test/java/com/vs/projects/SqsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
